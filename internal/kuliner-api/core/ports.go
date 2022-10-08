c46d6f8a5b4605388964c9c2d4b52b280245cbf9
fathisiddiqi <service.fathisiddiqi@gmail.com>
2022-10-08 15:35:44 +0700

    Rename UpdateFood port and add UpdateFood service

--- internal/kuliner-api/core/ports.go
+++ internal/kuliner-api/core/ports.go
@@ -6,5 +6,5 @@ type Storage interface {
 	IndexFood(ctx context.Context, food Food) error
 	DeleteFood(ctx context.Context, id string) error
 	SearchFood(ctx context.Context, query string, size int) ([]Food, error)
-	updateFood(ctx context.Context, id string, food Food) error
+	UpdateFood(ctx context.Context, id string, food Food) error
 }
--- internal/kuliner-api/core/service.go
+++ internal/kuliner-api/core/service.go
@@ -11,6 +11,7 @@ type Service interface {
 	IndexFood(ctx context.Context, input FoodInput) (*Food, error)
 	DeleteFood(ctx context.Context, id string) error
 	SearchFoods(ctx context.Context, query string) ([]Food, error)
+	UpdateFood(ctx context.Context, id string, input FoodInput) (*Food, error)
 }
 
 type Config struct {
@@ -45,6 +46,23 @@ func (s *service) IndexFood(ctx context.Context, input FoodInput) (*Food, error)
 	err = s.storage.IndexFood(ctx, food)
 	if err != nil {
 		return nil, fmt.Errorf("unable to index food into storage due: %w", err)
+	}
+	return &food, nil
+}
+
+func (s *service) UpdateFood(ctx context.Context, id string, input FoodInput) (*Food, error) {
+	err := input.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	food := Food{
+		Name:        input.Name,
+		Description: input.Description,
+	}
+	err = s.storage.UpdateFood(ctx, id, food)
+	if err != nil {
+		return nil, fmt.Errorf("unable to update food in storage due to: %w", err)
 	}
 	return &food, nil
 }
